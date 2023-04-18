13accf047aabc1c0f8c01f136d32792c3fd1f4f6
Hossein Fatemi <hfatemi1997@gmail.com>
2023-04-18 22:09:34 +0330

    fix: change origin

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://www.bilyo.ir',
+    origin: true,
     credentials: true,
     exposedHeaders: 'Access-Control-Allow-Credentials',
   });
